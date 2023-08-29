8673ee407e9acaf7165492d56a0012f4712214e3
deomaius <samuel.jj.gosling@gmail.com>
2023-08-29 16:51:20 +0000

    ownership test

--- test/CreditOracle.t.sol
+++ test/CreditOracle.t.sol
@@ -2,19 +2,27 @@ pragma solidity 0.8.15;
 
 import { Test } from '@forge-std/Test.sol';
 import { CreditOracle } from '@core/CreditOracle.sol';
+import { ICreditRegistry } from '@interfaces/ICreditRegistry.sol';
 
-contract CreditOracleTest {
+contract CreditOracleTest is Test {
 
 	CreditOracle oracle;
 
+	address MARKET_ADDRESS = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
 	address REGISTRY_ADDRESS = 0x95A2b07eB236110719975625CeE026ffe2b02799;
  	
-	function setUp() {
+	function setUp() public {
 		oracle = new CreditOracle(REGISTRY_ADDRESS);
 	}
 
-	function testOwnership() {}
+	function testOwnership() public {
+		/*  ------ REGISTRY ------ */
+		vm.startPrank(REGISTRY_ADDRESS);
+			oracle.log(MARKET_ADDRESS, ICreditRegistry.Term.ONE_MONTHS, 0);
+		vm.stopPrank();
+		/*  --------------------- */
+	}
 
-	function testQueryTimeWeighted() {}
+	function testQueryTimeWeighted() public {}
 
 }
