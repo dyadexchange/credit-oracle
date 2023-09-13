421c3716ab24f76d48cbba04d5efe6d8414a937b
deomaius <samuel.jj.gosling@gmail.com>
2023-09-13 14:51:57 +0000

    correct query test

--- test/CreditOracle.t.sol
+++ test/CreditOracle.t.sol
@@ -6,50 +6,49 @@ import { ICreditRegistry } from '@interfaces/ICreditRegistry.sol';
 
 contract CreditOracleTest is Test {
 
-	CreditOracle oracle;
+    CreditOracle oracle;
 
-	address MARKET_ADDRESS = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
-	address REGISTRY_ADDRESS = 0x95A2b07eB236110719975625CeE026ffe2b02799;
- 	
-	function setUp() public {
-		oracle = new CreditOracle(REGISTRY_ADDRESS);
-	}
+    address MARKET_ADDRESS = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    address REGISTRY_ADDRESS = 0x95A2b07eB236110719975625CeE026ffe2b02799;
+    
+    function setUp() public {
+        oracle = new CreditOracle(REGISTRY_ADDRESS);
+    }
 
-	function testOwnership() public {
-		_log_entry(0);
-	}
+    function testOwnership() public {
+        _log_entry(0);
+    }
 
-	function testQueryTimeWeighted() public {
-		uint256 startTimestamp = block.timestamp;
-		uint256[7] memory interest_rates = [
-			uint256(100 gwei),
-			uint256(101 gwei),
-			uint256(105 gwei),
-			uint256(101 gwei),
-			uint256(100 gwei),
-			uint256(99 gwei),
-			uint256(105 gwei)
-		];
+    function testQueryTimeWeighted() public {
+        uint256[7] memory interest_rates = [
+            uint256(100 gwei),
+            uint256(101 gwei),
+            uint256(105 gwei),
+            uint256(101 gwei),
+            uint256(100 gwei),
+            uint256(99 gwei),
+            uint256(105 gwei)
+        ];
 
-		for (uint256 x = 0; x < interest_rates.length; x++) {
-			_log_entry(interest_rates[x]);
+        for (uint256 x = 0; x < interest_rates.length; x++) {
+            _log_entry(interest_rates[x]);
 
-			vm.warp(block.timestamp + 1 weeks);
-		}
+            vm.warp(block.timestamp + 1 weeks);
+        }
 
-		(uint256 value, uint256 delta, uint256 ts) = oracle.queryTimeWeighted(
-			MARKET_ADDRESS,
-			ICreditRegistry.Term.ONE_MONTHS,
-			startTimestamp,
-			7
-		);
-	}
+        (uint256 value, uint256 delta, uint256 ts) = oracle.queryTimeWeighted(
+            MARKET_ADDRESS,
+            ICreditRegistry.Term.ONE_MONTHS,
+            block.timestamp - 1 weeks,
+            7
+        );
+    }
 
-	function _log_entry(uint256 rate) public {
-		/*  ------ REGISTRY ------ */
-		vm.startPrank(REGISTRY_ADDRESS);
-			oracle.log(MARKET_ADDRESS, ICreditRegistry.Term.ONE_MONTHS, rate);
-		vm.stopPrank();
-		/*  --------------------- */
-	}
+    function _log_entry(uint256 rate) public {
+        /*  ------ REGISTRY ------ */
+        vm.startPrank(REGISTRY_ADDRESS);
+            oracle.log(MARKET_ADDRESS, ICreditRegistry.Term.ONE_MONTHS, rate);
+        vm.stopPrank();
+        /*  --------------------- */
+    }
 }
