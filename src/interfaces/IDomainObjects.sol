9846131069d1e13ba22ac0ce4a35861ed584fa28
deomaius <samuel.jj.gosling@gmail.com>
2024-11-07 19:58:47 +0000

    introduce perpetual term type

--- src/interfaces/IDomainObjects.sol
+++ src/interfaces/IDomainObjects.sol
@@ -2,6 +2,6 @@ pragma solidity ^0.8.13;
 
 interface IDomainObjects {
     
-    enum Term { ONE_MONTHS, THREE_MONTHS, SIX_MONTHS, TWELVE_MONTHS }
+    enum Term { ONE_MONTHS, THREE_MONTHS, SIX_MONTHS, TWELVE_MONTHS, PERPETUAL }
 
 }
